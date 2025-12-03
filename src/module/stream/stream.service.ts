3856485b315c256e0b6a61da98779123d4976f79
Plisa <software3@pollinisrl.it>
2025-12-03 13:44:01 +0100

    add from work

--- src/module/stream/stream.service.ts
+++ src/module/stream/stream.service.ts
@@ -1,17 +1,46 @@
+import type { Prisma } from '@/prisma/generated';
 import { PrismaService } from '@/src/core/prisma/prisma.service';
 
+import { FiltersInput } from './inputs/filter.input';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class StreamService {
 	constructor(private readonly prismaService: PrismaService) {}
 
-	async findAll() {
+	async findAll(input: FiltersInput = {}) {
+		const { take, skip, searchTerm } = input;
+
+		const whereClause = searchTerm
+			? this.findBySearchTermFilter(searchTerm)
+			: undefined;
 		const streams = await this.prismaService.stream.findMany({
-			where: { user: { isDeactivated: false } },
-			include: { user: { where: { isDeactivated: false } } },
+			take: take ?? 12,
+			skip: skip ?? 0,
+			where: { user: { isDeactivated: false }, ...whereClause },
+			include: { user: true },
 		});
 
 		return streams;
+	}
+
+	private findBySearchTermFilter(
+		searchTerm: string,
+	): Prisma.StreamWhereInput {
+		return {
+			OR: [
+				{
+					title: {
+						contains: searchTerm,
+						mode: 'insensitive',
+					},
+				},
+				{
+					user: {
+						username: { contains: searchTerm, mode: 'insensitive' },
+					},
+				},
+			],
+		};
 	}
 }
